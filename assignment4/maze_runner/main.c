7ed60b553b936ec81e06d0448c4a493a3684f3dd
KangByung wook <byungwook@KangByungui-MacBook-Pro.local>
2016-04-04 20:03:52 +0900

    일단 구현은 됨, 코드 정리 필요

--- assignment4/maze_runner/main.c
+++ assignment4/maze_runner/main.c
@@ -4,21 +4,27 @@
 #include "maze.h"
 
 int main(){
-	int i, j;
-	for(i = 0 ; i < 4 ; i++){
-		printf("%d순위 : %d %d\n", i, moves[i].ver, moves[i].hor);
-	}
+	int i, j, move_status;
 	
-	print_maze_map();
+	print_maze_map(maze_map_original, 10, 10, 3, 3);
 	
 	printf("미로찾기를 시작합니다\n");
 	
+	// 시작 위치 세팅 
 	trace start = {1,1,0};
 	maze_map[start.row][start.col] = 1;
 	Push(&top, start);
-	int move_status = move(stack[top]);
-	while(move_status != 0){
-		move_status = move(stack[top]);
+	
+	do{
+		move_status = move(stack[top]);	
+	}
+	while(move_status != 0 && !isEmpty(stack));
+	
+	if(isEmpty(stack)){
+		printf("도저히 방법이 없습니다\n");
+	}
+	else{
+		printf("게임을 무사히 마쳤습니다.\n");
 	}
 	
 	return 0;	
--- assignment4/maze_runner/maze.h
+++ assignment4/maze_runner/maze.h
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// sleep를 위한 헤더파일 
+#include <windows.h>
+
 #include "stack.h"
+
+#define COL_SIZE 10
+#define ROW_SIZE 10
 
 // 이동시 좌표 변화 정보가 담긴 데이터 타입
 // 예를들어 북쪽으로 이동하는 경우 ver = -1, hor = 0; 
@@ -28,28 +34,51 @@ int maze_map[10][10] = {{1,1,1,1,1,1,1,1,1,1},
 				   {1,1,1,0,1,1,1,1,1,1}, 
 				   {1,1,1,0,1,1,1,1,1,1}, 
 				   {1,1,0,0,0,0,0,0,0,1}, 
-				   {1,1,0,1,1,1,1,99,1,1}, 
+				   {1,1,0,1,1,1,1,100,1,1}, 
 				   {1,1,0,0,0,0,1,1,1,1}, 
-				   {1,1,1,1,1,0,0,0,100,1},
+				   {1,1,1,1,1,0,0,0,0,1},
 				   {1,1,1,1,1,1,1,1,1,1}} ;
+int maze_map_original[10][10] = {{1,1,1,1,1,1,1,1,1,1}, 
+							   {1,0,1,1,1,1,1,1,1,1}, 
+							   {1,0,0,0,0,0,0,0,1,1}, 
+							   {1,1,1,0,1,1,1,1,1,1}, 
+							   {1,1,1,0,1,1,1,1,1,1}, 
+							   {1,1,0,0,0,0,0,0,0,1}, 
+							   {1,1,0,1,1,1,1,100,1,1}, 
+							   {1,1,0,0,0,0,1,1,1,1}, 
+							   {1,1,1,1,1,0,0,0,0,1},
+							   {1,1,1,1,1,1,1,1,1,1}} ;
 int dragon_catch = 0; 
-int arrival_befor_dragon_catch = 0;
+int arrival_before_dragon_catch = 0;
+
 // 헤더 파일의 maze_map을 출력해주는 함수
 // TODO 파일에서 입력받을 경우 함수 구조를 변경해야할 필요성. 
-void print_maze_map(){
+void print_maze_map(int (*map)[COL_SIZE], int height, int width, int current_row, int current_col){
+	int tmp_map[height][width];
 	int i,j;
-	for(i = 0 ; i < 10 ; i++){
-		for(j = 0 ; j < 10; j++){
-			printf("%3d ", maze_map[i][j]);
+	for(i = 0 ; i < height ; i++){
+		for(j = 0 ; j < width; j++){
+			tmp_map[i][j] = map[i][j];
+			if(i == current_row && j == current_col)tmp_map[i][j] = 77;
+			printf("%3d ", tmp_map[i][j]);
 		}
 		printf("\n");
 	}
+	
 }
 
 // 지도와 현재 위치를 기반으로 이동방향을 리턴받는 함수 
 int decide_direction(trace current_location){
 	int dir = 0;
-	while(maze_map[current_location.row + moves[dir].ver][current_location.col + moves[dir].hor] == 1 && dir < 4)dir++;
+	
+	if(arrival_before_dragon_catch){ 
+		while((maze_map[current_location.row + moves[dir].ver][current_location.col + moves[dir].hor] == 1 || maze_map[current_location.row + moves[dir].ver][current_location.col + moves[dir].hor] == 2 || maze_map[current_location.row + moves[dir].ver][current_location.col + moves[dir].hor] == 100) && dir < 4)dir++;
+	}
+	else{
+		while(maze_map[current_location.row + moves[dir].ver][current_location.col + moves[dir].hor] == 1 && dir < 4)dir++;
+	}
+		
+	
 	if(dir == 4)return -1;
 	else return dir;
 } 
@@ -57,35 +86,82 @@ int decide_direction(trace current_location){
 int move(trace current_location){
 	// 현재 위치로부터 움직일 방향을 판단
 	int dir = decide_direction(current_location);
+	
+	// 다음으로 갈 row 
 	int next_row = current_location.row+moves[dir].ver;
+	// 다음 위치의 col 
 	int next_col = current_location.col+moves[dir].hor;
 	
-	// 도착지에 도착한 경우 
-	if(maze_map[next_row][next_col] == 100){
-		printf("목적지에 도착했습니다\n"); 
-		return 0;
-	}
+	
 	
 	//더이상 움직일 곳이 없는 경우 
 	//스택 pop후 top에 위치한 trace정보로 이동 판단 
 	if(dir == -1){
+		if(arrival_before_dragon_catch == 1){
+			printf("현재 위치에 2를 표시합니다\n");
+			maze_map[stack[top].row][stack[top].col] = 2;
+		}
 		Pop(&top);
+		
 		printf("뒤로가기 : ");
 		printf("현재 위치: (%d, %d)\n", stack[top].row, stack[top].col); 
+		
+		// 용을 잡지 않고 도착지에 도착한 경우 뒤로 가면서 해당 자리에 2를 표시 
+		
+		
+		
+		print_maze_map(maze_map_original, 10, 10, stack[top].row, stack[top].col);
+		Sleep(300);
+		system("cls");
 		return -1;
 	} 
 	//움직일 곳이 있는 경우
 	//스택에 다음 이동 위치정보를 push 
 	else{
+		
+		if(arrival_before_dragon_catch == 1){
+			printf("arrival_before_dragon_catch를 0으로 바꿈\n");
+			arrival_before_dragon_catch = 0;	
+		} 
 		trace new_data = {next_row, next_col,dir};
 		Push(&top, new_data);
+		
 		// 이동 위치 1로 바꿔주기
 		printf("전진 : ");
 		printf("현재 위치: (%d, %d)\n", stack[top].row, stack[top].col); 
 		if(maze_map[next_row][next_col]==99){
+			dragon_catch = 1;
 			printf("용을 잡았습니다\n");
 		}
-	
+		// 도착지에 도착한 경우 
+		if(maze_map[next_row][next_col] == 100){
+			// 용을 잡지 않고 목적지에 도착한 경우
+			if(!dragon_catch){
+				arrival_before_dragon_catch = 1;
+				printf("용을 잡지 않고 목적지에 도착했습니다\n되돌아가십시오");
+				
+				Pop(&top);
+				printf("뒤로가기 : ");
+				printf("현재 위치: (%d, %d)\n", stack[top].row, stack[top].col); 
+				
+				// 용을 잡지 않고 도착지에 도착한 경우 뒤로 가면서 해당 자리에 2를 표시 
+				if(arrival_before_dragon_catch == 1){
+					printf("현재 위치에 2를 표시합니다\n");
+					maze_map[stack[top].row][stack[top].col] = 2;
+				} 
+				
+				print_maze_map(maze_map_original, 10, 10, stack[top].row, stack[top].col);
+				Sleep(300);
+				system("cls");
+				
+				return -1;
+			} 
+			printf("게임이 종료됩니다\n"); 
+			return 0;
+		}
+		print_maze_map(maze_map_original, 10, 10, stack[top].row, stack[top].col);
+		Sleep(300);
+		system("cls");
 		maze_map[next_row][next_col] = 1; 
 		return 1;
 	} 
