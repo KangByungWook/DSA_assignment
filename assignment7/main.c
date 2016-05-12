9deabb9601e04091cc0e192c8c1eea025dc3bc47
KangByung wook <byungwook@KangByungui-MacBook-Pro.local>
2016-05-12 16:49:55 +0900

    add comment done

--- assignment7/main.c
+++ assignment7/main.c
@@ -10,43 +10,46 @@
 #define nMAX 60000
 
 
-
+// 중복없는 난수를 생성하는 함수
+// 저장될 리스트(list)와 중복을 체크하는 리스트(bCheckExistOfNum), 그리고 총 갯수(number)를 매개변수로 받는다 
 void generate_random_number(int* list,int* bCheckExistOfNum,  int number){
 	int i;
-	for(i = 0 ; i < number ; i++){
+	for(i = 0 ; i < number ; i++){				// list와 bCheckExistOfNum 배열 요소 0으로 초기화 
 		bCheckExistOfNum[i] = 0;
 		list[i] = 0;
 	}
-	for(i=0 ; i<number ; )                    //번호가 6개 생성될 때까지 돈다.
+	for(i=0 ; i<number ; )                    //번호가 number개 생성될 때까지 돈다.
     {	
-    	int nTemp = rand()%number;                  //0~44 범위의 번호를 생성한다.
+    	int nTemp = rand()%number;                  //0부터 number까지의 번호를 생성한다.
     	
 	    
         if( bCheckExistOfNum[nTemp] == FALSE )  //중복 여부 판단
         {
         	bCheckExistOfNum[nTemp] = TRUE;     //번호가 중복 생성되지 않게 존재 여부를 true로 한다.
-            list[i] = nTemp+1;             //+1을 안해주면 범위가 0~44이다.
+            list[i] = nTemp+1;
             ++i;                                //증감 연산을 for문에서 하지 않고 여기서 한다.
         }
         else{
-        	if(number > 32767 && bCheckExistOfNum[nTemp + (number-32767 - 1)] == FALSE){
+        	// 난수가 0부터 32767까지 밖에 생성이 되지 않기 때문에 따로 처리해준다. 
+        	if(number > 32767 && bCheckExistOfNum[nTemp + (number-32767 - 1)] == FALSE){	
         		bCheckExistOfNum[nTemp + (number-32767) - 1] = TRUE;     //번호가 중복 생성되지 않게 존재 여부를 true로 한다.
-            	list[i] = nTemp + (number-32767);             //+1을 안해주면 범위가 0~44이다.
+            	list[i] = nTemp + (number-32767);
           		++i;                                //증감 연산을 for문에서 하지 않고 여기서 한다.
 			}
 		}
         
     }
-    for(i = 0 ; i < number; i++){
+    for(i = 0 ; i < number; i++){				// 생성된 난수를 출력한다. 
     	printf("%d ", list[i]);
 	}
 }
 
+// 정해진 정렬을 수행하고 수행 시간을 리턴해주는 함수 
 float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number){
-	time_t startTime,endTime;
+	time_t startTime,endTime;	// 함수 수행 시작시간, 종료시간 
 	int i; 
-	float gap;
-	switch(sort_code){
+	float gap;					// 총 함수 수행시간 
+	switch(sort_code){			// 정렬 코드에 따라서 다른 정렬을 수행한다 
 		//삽입 정렬의 경우 
 		case 1:
 			printf("난수 %d개를 생성 중입니다...\n", number);
@@ -79,7 +82,7 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
             Sleep(3000);
             printf("정렬 알고리즘을 시작합니다..\n");
             startTime = clock();
-			selectionSort(randomNum, number);
+			selectionSort(randomNum, number);		// 선택 정렬 실행. 
 			endTime = clock();
 			printf("정렬 알고리즘이 완료되었습니다.\n");
 			gap=(float)(endTime-startTime)/(CLOCKS_PER_SEC); //계산
@@ -93,7 +96,7 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
 			printf("\n정렬이 완료되었습니다.\n");
 			Sleep(3000); 
 			return gap;
-			
+		// 합병 정렬의 경우	
 		case 3:
 			printf("난수 %d개를 생성 중입니다...\n", number);
 			Sleep(3000);
@@ -102,7 +105,7 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
             Sleep(3000);
             printf("정렬 알고리즘을 시작합니다..\n");
             startTime = clock();
-			mergeSort(randomNum, 0, number);
+			mergeSort(randomNum, 0, number);			// 합병 정렬 실행. 
 			endTime = clock();
 			printf("정렬 알고리즘이 완료되었습니다.\n");
 			gap=(float)(endTime-startTime)/(CLOCKS_PER_SEC); //계산
@@ -116,7 +119,7 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
 			printf("\n정렬이 완료되었습니다.\n");
 			Sleep(3000); 
 			return gap;
-			
+		// 힙 정렬의 경우	
 		case 4:
 			printf("난수 %d개를 생성 중입니다...\n", number);
 			Sleep(3000);
@@ -125,7 +128,7 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
             Sleep(3000);
             printf("정렬 알고리즘을 시작합니다..\n");
             startTime = clock();
-			Heap_Sort(randomNum, number);
+			Heap_Sort(randomNum, number);			// 힙 정렬 실행. 
 			endTime = clock();
 			printf("정렬 알고리즘이 완료되었습니다.\n");
 			gap=(float)(endTime-startTime)/(CLOCKS_PER_SEC); //계산
@@ -146,11 +149,10 @@ float do_sort(int sort_code, int* randomNum, int* bCheckExistOfNum,  int number)
 
 int main( )
 {
-    //변수 생성 및 초기화 부분
     srand( (unsigned)time(NULL) );              //srand로 초기화
   	
-    int randomNum[nMAX];               //생성된 로또 번호를 저장할 변수
-    int bCheckExistOfNum[nMAX] = {0};     //생성된 번호가 중복인지 체크할 변수(인덱스)
+    int randomNum[nMAX];              		 //생성된 난수를 저장할 변수
+    int bCheckExistOfNum[nMAX] = {0};    	 //생성된 난수가 중복인지 체크할 변수(인덱스)
     int i,j;
     
   	float gap;
@@ -169,37 +171,43 @@ int main( )
         printf("q : 종료\n");
         printf("무엇을 하시겠습니까? : ");
         scanf("%s", cmd);
+        // 삽입 정렬의 경우 
         if(!strcmp(cmd,"1")){
             printf("삽입정렬 선택!\n");
             strcpy(sorting_name, "삽입정렬");
             sort_code = 1;
         }
+        // 선택 정렬의 경우 
         else if(!strcmp(cmd,"2")){
             printf("선택정렬 선택!\n");
             strcpy(sorting_name, "선택정렬");
             sort_code = 2;
         }
+        // 합병 정렬의 경우 
         else if(!strcmp(cmd,"3")){
             printf("합병정렬 선택!\n");
             strcpy(sorting_name, "합병정렬");
             sort_code = 3;
         }
+        // 힙 정렬의 경우 
         else if(!strcmp(cmd,"4")){
             printf("힙정렬 선택!\n");
             strcpy(sorting_name, "힙정렬");
             sort_code = 4;
         }
-        else if(!strcmp(cmd,"q")) break;
+        else if(!strcmp(cmd,"q")) break;			// q를 입력하면 종료 
         else{
             printf("올바른 코드를 입력해주세요\n");
             continue;
         }
         
+        // 각각 10000개, 20000개, 30000개, 40000개, 50000개, 60000개인 경우를 실행한다.  
         for(i = 0 ; i< 6; i++){
         	gap_result_list[i] = do_sort(sort_code, randomNum, bCheckExistOfNum, (i+1)*10000);
 		}
         
-        system("cls");
+        // 실험 결과를 출력한다. 
+        system("cls");			// 커맨드 창 클리어 
         printf("-----------%s 실험 결과--------------\n",sorting_name);
         for(i = 0 ; i < 6 ; i++){
         	printf("%d개 정렬 수행 시간 : %f초\n", (i+1)*10000, gap_result_list[i]);
--- assignment7/sort.c
+++ assignment7/sort.c
@@ -8,28 +8,21 @@ void SWAP(int a[], int b[]){
 	*b = tmp;
 }
 
-//
-void bubbleSort(int list[], int n){
-	int i, j, tmp;
-	for(i = 0 ; i < n ; i++){
-		for(j = 0; j < n - i - 1; j++){
-			if(list[j] > list[j+1]){
-				SWAP(list+j, list+j+1);
-			}
-		}
-	}
-}
-
-
 void insertSort(int list[], int n){
 	int i, j , key;
 	for(i = 1 ; i < n ; i++){
-		key = list[i];
+		// key: 비교할 값 
+		key = list[i];				
+		
+		// 인덱스 0의 위치요소부터 탐색하면서 key값이 들어갈 적절한 위치를 탐색 
 		for(j = i-1 ; j >= 0 ; j--){
-			if(list[j] < key){
+			// 인덱스를 하나씩 내려가면서 탐색
+			// 만약 자기 자신보다 큰 배열 요소를 찾으면 해당 요소의 다음 위치에 key값을 넣는다 
+			if(list[j] < key){		
 				list[j+1] = key;
 				break;
 			}
+			// 아닌 경우 위치를 바꾼다 
 			else{
 				list[j+1] = list[j];
 				list[j] = key;
@@ -46,6 +39,7 @@ void selectionSort(int *list, const int n)
     for (i = 0; i < n - 1; i++)
     {
         indexMin = i;
+        // 최소값을 찾는 과정 
         for (j = i + 1; j < n; j++)
         {
             if (list[j] < list[indexMin])
@@ -53,22 +47,26 @@ void selectionSort(int *list, const int n)
                 indexMin = j;
             }
         }
+        // 최소값을 찾으면 값을 교체한다 
         temp = list[indexMin];
         list[indexMin] = list[i];
         list[i] = temp;
     }
 }
 
-//mergeSort 시작 
+ 
 void mergeSort(int list[], int left, int right){
+	// middle -> left 배열과 right배열의 경계 
 	int middle = (left+right) / 2;
 	if(left < right){
+		// left배열과 right배열에 대해 각각 재귀적으로 합병정렬을 실행 
 		mergeSort(list, left, middle);
 		mergeSort(list, middle+1, right);
-		merge(list, left, middle, right);
+		merge(list, left, middle, right);	// 분산뒨 배열을 합쳐주는 함수 실행 
 	}
 }
 
+// 분산된 배열을 병합시켜주는 함수 
 void merge(int* array, int left, int mid, int right){
 	int i, j, k, m; 
 
@@ -108,11 +106,11 @@ void merge(int* array, int left, int mid, int right){
 	    array[m] = tempArray[m];
 	}
 }
-// mergerSort 끝
-//
 
- 
-void Min_Heap(int parm_data[], int parm_start, int parm_count)
+
+
+// 배열을 Heap의 구조로 바꿔주는 함수 
+void Max_Heap(int parm_data[], int parm_start, int parm_count)
 {
     int node_left, node_right, node_current, node_start, node_index;
     if(parm_count - parm_start < 2) return;
@@ -122,13 +120,22 @@ void Min_Heap(int parm_data[], int parm_start, int parm_count)
         node_index = node_current;
         node_start = node_current;
         while(node_start*2+1 < parm_count){
+        	// 왼쪽 자식노드는 부모 노드 인덱스*2 
+        	// 0부터 시작하므로 1을 더해준다 
             node_left = node_start*2+1;
+            
+            // 오른쪽 자식노드는 부모 노드 인덱스*2 + 1
+            // 0부터 시작하므로 1을 더해준다 
             node_right = node_start*2+2;
+            
+            // 왼쪽 노드부터 시작한다. 
             node_start = node_left;
  
+ 			// 왼쪽 노드와 오른쪽 노드를 비교해서 큰 쪽으로 node_start를 지정 
             if(node_right < parm_count && parm_data[node_right] >= parm_data[node_left]){
                 node_start = node_right;
             }
+            // 부모노드보다 자식노드가 더 크면 바꿔준다
             if(parm_data[node_start] > parm_data[node_index]){
                 SWAP(&parm_data[node_index], &parm_data[node_start]);
                 node_index = node_start;
@@ -137,14 +144,17 @@ void Min_Heap(int parm_data[], int parm_start, int parm_count)
         --node_current;
     }
 }
- 
+
+// 힙정렬 함수 
 void Heap_Sort(int parm_data[], int parm_count)
-{
-    Min_Heap(parm_data, parm_count/2-1, parm_count);
+{	
+    Max_Heap(parm_data, parm_count/2-1, parm_count);
     while(parm_count > 0){
         --parm_count;
+        // parm_data[0]에 있는 숫자가 가장 크므로 이를 맨뒤로 보내준다 
         SWAP(&parm_data[0], &parm_data[parm_count]);
-        Min_Heap(parm_data, 0, parm_count);
+        // 배열을 다시 Heap 구조로 만들어준다 
+        Max_Heap(parm_data, 0, parm_count);
     }
 }
 
